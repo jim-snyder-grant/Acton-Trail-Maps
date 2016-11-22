a95323966d0a4f9af8828217c4f1f96ce0b8e3e4
Jim Snyder-Grant <jimsg@newview.org>
2016-11-22 10:22:21 -0500

    building a PDF for the wesbite - includes title, and a larger version of the guidebook map, to fit in in a 8.5 X 11 PDF with half-inch margins

---
+++ ActonCanoeLaunch/pdf.sh
@@ -0,0 +1,6 @@
+convert -size 2550x3300 canvas:white temp.png
+composite  \( CanoeLaunchMapForGuidebook.png  -geometry "160%" \) -gravity south -geometry "+0+150"  temp.png temp.png
+composite title.png -gravity north -geometry +0+200   temp.png temp.png
+convert temp.png -size 2550x3300 -units 'PixelsPerInch' -density 300 mActonCanoeLaunch.pdf
+
+
---
+++ ActonCanoeLaunch/title.sh
@@ -0,0 +1,4 @@
+         convert -background white -fill black  -font Nimbus-Sans-L-Bold \
+          -size 2000x200  -pointsize 120  -gravity center \
+          label:"Acton Canoe Launch"     title.png
+
