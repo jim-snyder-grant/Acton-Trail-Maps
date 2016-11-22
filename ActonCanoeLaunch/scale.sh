d9f9150ccf7bcf7ad568acdfb6c125ca34bd7a9c
Jim Snyder-Grant <jimsg@newview.org>
2016-11-22 11:21:11 -0500

    changed 'foot' label positioning to be relative to east edge, to balance out left/right white space

--- ActonCanoeLaunch/scale.sh
+++ ActonCanoeLaunch/scale.sh
@@ -4,7 +4,7 @@ convert -size 1060x70 xc:white  -font Nimbus-Sans-L-Bold\
       -draw 'fill black stroke black stroke-width 3 rectangle 254,40 376,70' \
       -draw 'fill black stroke black stroke-width 3 rectangle 498,40 986,70' \
       scale.png
-convert scale.png  -gravity west -pointsize 28 -annotate +990+20 'Feet' scale.png
+convert scale.png  -gravity east -pointsize 28 -annotate +10+20 'Feet' scale.png
 convert scale.png  -gravity center -pointsize 30 -annotate -520-15  '0' scale.png
 convert scale.png  -gravity center -pointsize 30 -annotate -398-15  '125' scale.png
 convert scale.png  -gravity center -pointsize 30 -annotate -276-15  '250' scale.png
