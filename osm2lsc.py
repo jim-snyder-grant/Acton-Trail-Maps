1943c6b5e241edf44c521e2773b6b26719b93cd6
Joe Holmes <Joseph.Holmes@alum.mit.edu>
2017-11-11 23:47:31 -0500

    Fixed most of the PEP8 style errors

--- osm2lsc.py
+++ osm2lsc.py
@@ -62,7 +62,7 @@ def ourSED(inPattern, outPattern, filename):
         fid, tempName = mkstemp()
         inContents = fin.read()
         outContents = re.sub(inPattern, outPattern, inContents,
-        flags=re.MULTILINE | re.IGNORECASE)
+                             flags=re.MULTILINE | re.IGNORECASE)
         os.write(fid, outContents)
         os.close(fid)
     os.remove(filename)
@@ -76,7 +76,7 @@ def ourCall(args):
     except subprocess.CalledProcessError, e:
         print "Execution failed wih code ", e.returncode
         print e.output
-        exit (e.returncode) 
+        exit(e.returncode)
     return output_text
 
 
@@ -86,19 +86,19 @@ for arg in args:
         # we were getting too-many-requests errors.
         sleep(time_delay)
 
-    # by default, extract lines. But exceptions are made below 
-    geometry = "lines"   
+    # by default, extract lines. But exceptions are made below
+    geometry = "lines"
     # we surpress special treatment for trails that are entirely outside Acton
     AREA_FILTER = "(area:"+ACTON_AREA_ID+")"
-    
+
     if arg == "bounds":
         # Some bounds are multipolygons stored in OSM as 'relation', others are
         # plain old 'way'. And then there's the canoe launch, which isn't
         # owned by the town of Acton.
         # We are in a transition away from landuse=conservation
-        KMLcolor = 'ffffffff'  
+        KMLcolor = 'ffffffff'
         filters = '(way('+CANOE_LAUNCH_ID+ ');relation[boundary=protected_area][owner~"Town Of Acton",i];way[boundary=protected_area][owner~"Town Of Acton",i];relation[leisure=nature_reserve][owner~"Town Of Acton",i];way[leisure=nature_reserve][owner~"Town Of Acton",i])'
-        AREA_FILTER=""
+        AREA_FILTER = ""
         geometry = "multipolygons"
         GOT_NEW_BOUNDS = True
     elif arg == "red":
@@ -108,17 +108,17 @@ for arg in args:
         KMLcolor = "ffff0000"
         filters = TRAILS_FILTER+'~"'+arg+'",i][name!~"'+SPECIAL_TRAIL+'"]'
     elif arg == "yellow":
-        KMLcolor =" ff00ffff"
+        KMLcolor = " ff00ffff"
         filters = TRAILS_FILTER+'~"'+arg+'",i]'
     elif arg == "green":
         KMLcolor = "ff00AA14"
         filters = TRAILS_FILTER+'~"'+arg+'",i][name!~"'+SPECIAL_TRAIL+'"]'
     elif arg == "othertrails":
-        KMLcolor="ffff00ff"
+        KMLcolor = "ffff00ff"
         # This is all trails outside of Acton & the trails without special
         # color names inside (but no private trails), plus one special trail.
-        filters='way[name="'+SPECIAL_TRAIL+'"]->.special; way[highway~"path|track|footway"][footway!~"sidewalk|crossing"][access!=private]->.everything;way[highway~"path|track"][access!=private][name!~"Red|Blue|Green|Yellow",i]->.innards;((.everything; - way(area:3601832779););.innards;.special;)'
-        AREA_FILTER=""
+        filters = 'way[name="'+SPECIAL_TRAIL+'"]->.special; way[highway~"path|track|footway"][footway!~"sidewalk|crossing"][access!=private]->.everything;way[highway~"path|track"][access!=private][name!~"Red|Blue|Green|Yellow",i]->.innards;((.everything; - way(area:3601832779););.innards;.special;)'
+        AREA_FILTER = ""
     elif arg == "town":
         KMLcolor = "ff00ff00"
         filters = 'area[wikipedia="en:Acton, Massachusetts"];rel(pivot)'
@@ -131,23 +131,22 @@ for arg in args:
     elif arg == "camping":
         KMLcolor = "643C9614"
         filters = 'node[tourism=camp_site]'
-        geometry = "points" 
+        geometry = "points"
     elif arg == "bct":
-        KMLcolor = "55FF78F0"  
+        KMLcolor = "55FF78F0"
         filters = 'relation[name~"Bay Circuit Trail"];(._;>;)->.a;way.a(42.433,-71.5,42.534,-71.384)'
         AREA_FILTER = ""
     elif arg == "bfrt":
-        KMLcolor = "501450FF"  
+        KMLcolor = "501450FF"
         filters = 'way[name="Bruce Freeman Rail Trail: Phase 2 (proposed)"]'
         AREA_FILTER = ""
     elif arg == helpArg:
         usage()
-        exit (0)
+        exit(0)
     else:
         print "ERROR: Invalid arg: " + arg
-        exit (1) 
- 
-	
+        exit(1)
+
     osmFile = arg + ".osm"
     kmlFile = arg + ".kml"
     backupFile = arg + ".old.kml"
@@ -155,94 +154,91 @@ for arg in args:
     # osmDriver = gdal.GetDriverByName("OSM")
     # kmlDriver = gdal.GetDriverByName("KML")
     # geojsonDriver = gdal.GetDriverByName("GeoJSON")
- 
+
     url = "http://overpass-api.de/api/interpreter?data="+ACTON_BBOX+filters+AREA_FILTER+";(._;>;);out body;"
     response = requests.get(url)
     while 429 == response.status_code or 504 == response.status_code:
         time_delay = time_delay+3
-        print "Too many requests: slowing down delay to ", time_delay , " seconds"
+        print "Too many requests: slowing down delay to ", time_delay, " seconds"
         if time_delay > 60:
             exit(1)
         sleep(time_delay)
         response = requests.get(url)
 
-    if 200 != response.status_code :
+    if 200 != response.status_code:
         print "Status code: ", response.status_code, " on this request"
-        print (url)
-        exit (1)
+        print(url)
+        exit(1)
     contents = response.text
     argFile = open(osmFile, 'w')
     argFile.write(contents)
     argFile.close()
-    
+
     if os.path.exists(osmFile):
         print("created " + osmFile)
     else:
         print("ERROR: Did not create " + osmFile)
         exit(1)
-    
+
     # Check to see if the OSM file has changed. We do this by simply running
     # a git diff. If it is exactly 7 lines long, then we only have one
     # changed line, which is the osm_base timestamp.
-    diff_bs    = ourCall(['git', 'diff', '-U0', osmFile])
-    diff_s     = diff_bs.decode('ASCII')
+    diff_bs = ourCall(['git', 'diff', '-U0', osmFile])
+    diff_s = diff_bs.decode('ASCII')
     diff_lines = diff_s.count('\n')
     if diff_lines == 7:
         print ("No changes")
         # If there are no changes, then restore the old file with git checkout
         ourCall(['git', 'checkout', osmFile])
-        
+
     else:
 
-        #save the old KML for comparing in qgis
+        # save the old KML for comparing in qgis
         if os.path.exists(kmlFile):
             if os.path.exists(backupFile):
                 os.remove(backupFile)
             os.rename(kmlFile, backupFile)
-    
-    
+
         ourCall(['ogr2ogr', '-f', 'KML', kmlFile, osmFile, geometry])
-   
+
         # now put in special color for looking at KML in google earth
-        ourSED("<color>........", "<color>"+KMLcolor, kmlFile)   
-    
+        ourSED("<color>........", "<color>"+KMLcolor, kmlFile)
+
         # convert to GeoJSON for importing into MapBox
         if os.path.exists(geojsonFile):
             os.remove(geojsonFile)
 
         ourCall(['ogr2ogr', '-f', 'GeoJSON', geojsonFile, osmFile, geometry])
-        
+
         # if it's the BCT, do post-processing to create a single segment from the individual ways
         if arg == "bct":
-            ourCall(['ogr2ogr', "temp", geojsonFile])    
+            ourCall(['ogr2ogr', "temp", geojsonFile])
             os.remove(geojsonFile)
             ourCall(['ogr2ogr', "-f", "GeoJSON", geojsonFile, "temp",
                      "-dialect", "sqlite", "-sql",
-                     "SELECT ST_LineMerge(ST_Collect(geometry)) FROM OGRGeoJSON"])    
-            shutil.rmtree("temp") 
+                     "SELECT ST_LineMerge(ST_Collect(geometry)) FROM OGRGeoJSON"])
+            shutil.rmtree("temp")
 
-        # ogr2ogr emits geoJSON v 1.0. MapBox needs the successor, RFC 7946. 
+        # ogr2ogr emits geoJSON v 1.0. MapBox needs the successor, RFC 7946.
         # The key difference is that we must remove the crs line
-        ourSED(r"^.crs.*$", "", geojsonFile)   
-    
-        # remove the colors from the names of Acton trails 
-        if arg in ['blue','red','green','yellow']:
+        ourSED(r"^.crs.*$", "", geojsonFile)
+
+        # remove the colors from the names of Acton trails
+        if arg in ['blue', 'red', 'green', 'yellow']:
             ourSED(r"\s?"+arg+r"\s?", "", geojsonFile)
-    
+
         # just checking and announcing results
         if os.path.exists(kmlFile):
-	    print("created " + kmlFile)
+            print("created " + kmlFile)
         else:
-	    print("ERROR: Did not create " + kmlFile)
-	    exit(1)
-    
-        if os.path.exists(geojsonFile):
-	    print("created " + geojsonFile)
-        else:
-	    print("ERROR: Did not create " + geojsonFile)
-	    exit(1)
+            print("ERROR: Did not create " + kmlFile)
+            exit(1)
 
-    
+        if os.path.exists(geojsonFile):
+            print("created " + geojsonFile)
+        else:
+            print("ERROR: Did not create " + geojsonFile)
+            exit(1)
+
     if GOT_NEW_BOUNDS:
         ourSED(" Conservation Land", "", "bounds.geojson")
-       
