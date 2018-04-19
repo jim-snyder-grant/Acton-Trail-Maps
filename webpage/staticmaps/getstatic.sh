b5ebf6ee7f8d9023d9fe6ccc1cb4f5f63a2e9b8c
Jim Snyder-Grant <jimsg@newview.org>
2018-04-19 12:45:00 -0400

    new staticmaps directory

---
+++ webpage/staticmaps/getstatic.sh
@@ -0,0 +1,20 @@
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.43315,42.47993,15.3,0,0/650x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >ActonArboretum.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.42929,42.43993,16.9,0,0/780x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >ActonCanoeLaunch.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.46301,42.49532,15.2,0,0/778x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >Bulette.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.39385,42.5034,15.6,0,0/1000x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >CampActon.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.44667,42.49986,15.2,0,0/778x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >GrassyPond.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.44969,42.46779,14.3,0,0/440x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >GreatHill.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.48206,42.48093,15.2,0,0/500x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >Guggins.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.47446,42.46192,14.1,0,0/300x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >HeathHen.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.48083,42.48801,15.6,0,0/480x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >Jenks.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.43562,42.50331,14.5,0,0/460x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >NagogHill.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.40899,42.50824,14.5,0,0/474x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >NashobaBrook.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.46149,42.46528,15.6,0,0/400x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >pacy.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.44035,42.45481,15.6,0,0/760x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >prattsbrook.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.39793,42.5135,14.4,0,0/540x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >RobbinsMill.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.40252,42.50172,14.1,0,0/370x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >SpringHill.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.4016,42.48533,15.1,0,0/440x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >stoneymeade.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.41679,42.47273,15.6,0,0/820x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >wetherbee.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.4196,42.51762,14.5,0,0/400x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >WillsHole.jpg
+wget -O - https://api.mapbox.com/styles/v1/jimsg/civ17sdex00l02io48dp99x1m/static/-71.4779,42.47897,16.1,0,0/400x600@2x?access_token=pk.eyJ1Ijoiamltc2ciLCJhIjoiNDhhdHdCZyJ9.ZV92MDJEE14leO3JMm89Yw >WrightHill.jpg
+
