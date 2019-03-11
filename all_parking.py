d8ee61ef494f39979294712b684a571f75bac9d2
Joe Holmes <Joseph.Holmes@alum.mit.edu>
2019-03-11 18:59:30 -0400

    Added tags: addr:* and surface to points, addr:*, capacity, and surface to areas.

--- all_parking.py
+++ all_parking.py
@@ -15,16 +15,25 @@ gdal.UseExceptions()
 OUTFILEBASE = "all_parking"
 
 # geoJSON writing prep
+CAPACITYKEY = "capacity"
 OSMKEY = "osm_id"
 OSMWAYKEY = "osm_way_id"
+SURFACEKEY = "surface"
+
 inExtension = r".geojson"
 GeoJSONdriver = ogr.GetDriverByName("GeoJSON")
 
 outfileJSON = GeoJSONdriver.CreateDataSource(OUTFILEBASE + '.geojson')
 outLayerJSON = outfileJSON.CreateLayer("OGRGeoJSON")
 
+capacityfield = ogr.FieldDefn(CAPACITYKEY, ogr. OFTString)
+outLayerJSON.CreateField(capacityfield)
+
 osmfield = ogr.FieldDefn(OSMKEY, ogr. OFTString)
 outLayerJSON.CreateField(osmfield)
+
+surfacefield = ogr.FieldDefn(SURFACEKEY, ogr. OFTString)
+outLayerJSON.CreateField(surfacefield)
 
 layerDefnJSON = outLayerJSON.GetLayerDefn()
 
@@ -40,13 +49,25 @@ for info in basenames:
     # read from source file
         geom = infeature.GetGeometryRef()
         try:
+            capacity = infeature.GetField(CAPACITYKEY)
+        except ValueError:
+            capacity = ""
+        try:
             osm_id = infeature.GetField(OSMWAYKEY)
         except ValueError:
             osm_id = infeature.GetField(OSMKEY)       
+        try:
+            surface = infeature.GetField(SURFACEKEY)
+        except ValueError:
+            surface = ""
                 
         outfeatureJSON = ogr.Feature(layerDefnJSON)
         outfeatureJSON.SetGeometry(geom)
         outfeatureJSON.SetField(OSMKEY, osm_id)
+        if (capacity):
+            outfeatureJSON.SetField(CAPACITYKEY, capacity)
+        if (surface):
+            outfeatureJSON.SetField(SURFACEKEY, surface)
                
         outLayerJSON.CreateFeature(outfeatureJSON)
         outfeatureJSON = None
